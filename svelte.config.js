d8e324d0684713b998ff8884c33d22e76b1ef0f1
LeoDog896 <LeoDog896@hotmail.com>
2022-05-04 14:37:04 +0000

    Add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,29 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
 		preprocess({
 			postcss: true
-		})
+		}),
+		mdsvex(mdsvexConfig)
 	],
 
 	kit: {
 		adapter: adapter(),
-    paths: {
-      base: "/webpage"
-    },
-    prerender: {
-      default: true
-    }
+		paths: {
+			base: '/webpage'
+		},
+		prerender: {
+			default: true
+		}
 	}
 };
 
