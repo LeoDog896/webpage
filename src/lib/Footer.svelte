9ee71364ac2b94ee6c7bf0bf58e4381e910bbd08
Tristan F <LeoDog896@hotmail.com>
2023-02-10 10:21:21 -0500

    style: fmt

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,3 +1,5 @@
 <footer class="mt-10 p-4 text-center fixed bottom-0 w-screen bg-[#B2CEDE]">
-	Made with ❤️ by Tristan | follow me on <a class="underline" href="https://github.com/LeoDog896">github</a>
+	Made with ❤️ by Tristan | follow me on <a class="underline" href="https://github.com/LeoDog896"
+		>github</a
+	>
 </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,71 +6,71 @@
 
 	interface DescriptiveLink extends Link {
 		description: string;
-		type: "project" | "security" | "article";
+		type: 'project' | 'security' | 'article';
 	}
 
 	const links: Link[] = [
 		{
-			name: "so",
-			href: "https://stackoverflow.com/users/7589775/leodog896"
+			name: 'so',
+			href: 'https://stackoverflow.com/users/7589775/leodog896'
 		},
 		{
-			name: "gh",
-			href: "https://github.com/LeoDog896"
+			name: 'gh',
+			href: 'https://github.com/LeoDog896'
 		},
 		{
-			name: "discord",
-			href: "https://discord.com/users/LeoDog896#0675"
+			name: 'discord',
+			href: 'https://discord.com/users/LeoDog896#0675'
 		},
 		{
-			name: "email",
-			href: "mailto:leodog896@gmail.com"
+			name: 'email',
+			href: 'mailto:leodog896@gmail.com'
 		}
-	]
+	];
 
-	const favoriteEmojis = ["🌼", "✨", "🍚"]
+	const favoriteEmojis = ['🌼', '✨', '🍚'];
 
 	const randomEmoji = (exceptForIndex = -1): string => {
-		const index = Math.floor(Math.random() * favoriteEmojis.length)
-		if (index === exceptForIndex) return randomEmoji(exceptForIndex)
-		return favoriteEmojis[index]
-	}
+		const index = Math.floor(Math.random() * favoriteEmojis.length);
+		if (index === exceptForIndex) return randomEmoji(exceptForIndex);
+		return favoriteEmojis[index];
+	};
 
 	const refreshEmoji = () => {
-		currentEmoji = randomEmoji(favoriteEmojis.indexOf(currentEmoji))
-	}
+		currentEmoji = randomEmoji(favoriteEmojis.indexOf(currentEmoji));
+	};
 
-	let currentEmoji = randomEmoji()
+	let currentEmoji = randomEmoji();
 
 	const stuff: DescriptiveLink[] = [
 		{
-			name: "CVE-2023-22499",
-			description: "my first reported security vulnerability! hope to find more.",
-			href: "https://github.com/denoland/deno/security/advisories/GHSA-mc52-jpm2-cqh6",
-			type: "security"
+			name: 'CVE-2023-22499',
+			description: 'my first reported security vulnerability! hope to find more.',
+			href: 'https://github.com/denoland/deno/security/advisories/GHSA-mc52-jpm2-cqh6',
+			type: 'security'
 		},
 		{
-			name: "deno-outdated",
-			description: "a tool to check for outdated dependencies in your deno project",
-			href: "https://github.com/LeoDog896/deno-outdated",
-			type: "project"
+			name: 'deno-outdated',
+			description: 'a tool to check for outdated dependencies in your deno project',
+			href: 'https://github.com/LeoDog896/deno-outdated',
+			type: 'project'
 		},
 		{
-			name: "how to make a lichess bot",
-			description: "i am very bad at chess. thus, what better to do than make a bot worse than me.",
-			href: "/article/lichess-bot",
-			type: "article"
+			name: 'how to make a lichess bot',
+			description: 'preface: i am very bad at chess.',
+			href: '/article/lichess-bot',
+			type: 'article'
 		}
-	]
+	];
 </script>
 
-<h1>leo 
-	<button class="text-like"
-		on:click={refreshEmoji}
-		on:keypress={refreshEmoji}
-	>{currentEmoji}</button>
+<h1>
+	leo
+	<button class="text-like" on:click={refreshEmoji} on:keypress={refreshEmoji}
+		>{currentEmoji}</button
+	>
 </h1>
-<p>i make & break things on computers</p>
+<p>i make & break things on the internet</p>
 <p>
 	{#each links as { name, href }, i}
 		<a {href}>{name}</a>
@@ -82,7 +82,7 @@
 
 {#each stuff as { name, description, href, type }}
 	<div class="item">
-		<a {href} class="{`link-${type}`}">
+		<a {href} class={`link-${type}`}>
 			<h2>{name}</h2>
 		</a>
 		<p><i>{description}</i></p>
