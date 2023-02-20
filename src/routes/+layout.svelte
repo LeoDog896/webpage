fed3aec19e2cf6fe649e7c8243fc5e2a9b462277
Tristan F <LeoDog896@hotmail.com>
2023-02-19 21:46:44 -0500

    feat: title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,8 @@
 	import 'prism-themes/themes/prism-material-oceanic.css';
 </script>
 
+<svelte:head>
+	<title>leo</title>
+</svelte:head>
+
 <slot />
---
+++ src/routes/article/+layout.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+  <meta name="author" content="LeoDog896" />
+</svelte:head>
+
+<a href="/" class="back">back</a>
+
+<slot/>
+
+<style lang="scss">
+  .back {
+	display: block;
+	margin: 10px 0;
+  }
+</style>
